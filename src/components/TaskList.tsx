c779789e235b29bbf2397c42a91a287df9950211
Rafael Gonzaga <rafagonzagag@gmail.com>
2021-09-21 22:55:24 -0300

    ajustes

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -42,12 +42,12 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas Tarefas</h2>
+        <h2>Minhas tasks</h2>
 
         <div className="input-group">
           <input 
             type="text" 
-            placeholder="Adicionar nova tarefa" 
+            placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
